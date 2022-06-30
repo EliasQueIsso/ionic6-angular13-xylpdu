6510fb5a26fee331f742f8630f3f51fd41a946fe
EliasQueIsso <elias2004souzam@gmail.com>
2022-06-29 23:25:04 -0300

    Reutilização de código com aperfeiçoamento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,41 @@ import { AlertController, ToastController } from '@ionic/angular';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name = 'Ionic 6 Angular ' + VERSION.major;
+  donate: number;
 
-  ionViewDidEnter() {
-    console.log('CRAP')
+  constructor(
+    public toastController: ToastController,
+    public alertController: AlertController
+  ) {}
+  async exibirToast(mensagem: string) {
+    const toast = await this.toastController.create({
+      message: mensagem,
+      duration: 2000,
+      color: 'success',
+    });
+    toast.present();
+  }
+
+  async alertaEntrada() {
+    const alert = await this.alertController.create({
+      header: 'Exemplo Input',
+      inputs: [
+        {
+          name: 'inputNome',
+          type: 'number',
+          placeholder: 'Digite seu nome',
+        },
+      ],
+      buttons: [
+        {
+          text: 'Ok',
+          handler: (valor: any) => {
+            this.donate = valor['inputNome'];
+            this.exibirToast(this.donate.toString());
+          },
+        },
+      ],
+    });
+    await alert.present();
   }
 }
